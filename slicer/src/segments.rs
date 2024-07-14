ef0c7c02001f428d004045414f787db665135737
Connor Slade <connor@connorcode.com>
2024-07-13 22:51:08 -0400

    Re-use transformed points for face intersections

--- slicer/src/segments.rs
+++ slicer/src/segments.rs
@@ -7,6 +7,7 @@ pub struct Segments {
     layer_height: f32,
 
     layers: Vec<Vec<usize>>,
+    transformed_points: Vec<Vector3<f32>>,
 }
 
 impl Segments {
@@ -19,20 +20,16 @@ impl Segments {
             .collect::<Vec<_>>();
 
         let layer_height = (max.z - min.z) / layer_count as f32;
-        let mut layers = vec![Vec::new(); layer_count + 2];
+        let mut layers = vec![Vec::new(); layer_count + 1];
 
         for face in 0..mesh.faces.len() {
             let (min_height, max_height) = minmax_triangle_height(mesh, &transformed_points, face);
             let (min_layer, max_layer) = (
                 ((min_height - min.z) / layer_height) as usize,
-                ((max_height - min.z) / layer_height) as usize,
+                ((max_height - min.z) / layer_height).round() as usize,
             );
 
-            for layer in layers
-                .iter_mut()
-                .take(max_layer + 2)
-                .skip(min_layer.saturating_sub(1))
-            {
+            for layer in layers.iter_mut().take(max_layer + 1).skip(min_layer) {
                 layer.push(face);
             }
         }
@@ -40,7 +37,9 @@ impl Segments {
         Self {
             start_height: min.z,
             layer_height,
+
             layers,
+            transformed_points,
         }
     }
 
@@ -49,7 +48,7 @@ impl Segments {
 
         let layer = (height - self.start_height) / self.layer_height;
         for &face in self.layers[layer as usize].iter() {
-            intersect_triangle(mesh, face, height, &mut out);
+            intersect_triangle(mesh, &self.transformed_points, face, height, &mut out);
         }
 
         out
@@ -70,17 +69,22 @@ fn minmax_triangle_height(mesh: &Mesh, points: &[Vector3<f32>], triangle: usize)
     )
 }
 
-fn intersect_triangle(mesh: &Mesh, face: usize, height: f32, out: &mut Vec<Vector3<f32>>) {
+fn intersect_triangle(
+    mesh: &Mesh,
+    points: &[Vector3<f32>],
+    face: usize,
+    height: f32,
+    out: &mut Vec<Vector3<f32>>,
+) {
     let face = mesh.faces[face];
-    let v0 = mesh.vertices[face[0] as usize];
-    let v1 = mesh.vertices[face[1] as usize];
-    let v2 = mesh.vertices[face[2] as usize];
+    let v0 = points[face[0] as usize];
+    let v1 = points[face[1] as usize];
+    let v2 = points[face[2] as usize];
 
     let (a, b, c) = (v0.z - height, v1.z - height, v2.z - height);
     let (a_pos, b_pos, c_pos) = (a > 0.0, b > 0.0, c > 0.0);
 
     let mut push_intersection = |a: f32, b: f32, v0: Vector3<f32>, v1: Vector3<f32>| {
-        let (v0, v1) = (mesh.transform(&v0), mesh.transform(&v1));
         let t = a / (a - b);
         let intersection = v0 + t * (v1 - v0);
         out.push(intersection);
