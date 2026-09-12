82e24dc4c3b5cae9cb67df70990ae61ab8a9645a
Connor Slade <connor@connorcode.com>
2026-09-12 17:30:39 -0400

    Mesh repair report

--- mslicer/src/task/mesh/repair.rs
+++ mslicer/src/task/mesh/repair.rs
@@ -1,7 +1,6 @@
 use clone_macro::clone;
 use common::progress::Progress;
-use slicer::mesh::Mesh;
-use tools::repair;
+use tools::repair::{self, RepairResult};
 use tracing::info;
 
 use crate::{
@@ -10,10 +9,11 @@ use crate::{
         BuildAccelerationStructures, MeshDefective, PollResult, Task, TaskApp, TaskStatus,
         thread::TaskThread,
     },
+    ui::{components::grid, popup::Popup},
 };
 
 pub struct MeshRepair {
-    handle: TaskThread<Mesh>,
+    handle: TaskThread<RepairResult>,
     progress: Progress,
 
     model: ModelId,
@@ -38,7 +38,7 @@ impl MeshRepair {
                 "{{ unwelded_vertices: {}, holes: {} }}",
                 result.unwelded_vertices, result.holes
             );
-            result.mesh
+            result
         }));
 
         Self {
@@ -53,13 +53,39 @@ impl Task for MeshRepair {
     fn poll(&mut self, app: &mut TaskApp) -> PollResult {
         self.handle
             .poll(app, "Failed to Repair Mesh")
-            .into_poll_result(|mesh| {
+            .into_poll_result(|result| {
                 let platform = app.project.slice_config.platform_size;
                 let Some(model) = app.project.model(self.model) else {
                     return PollResult::complete();
                 };
 
-                model.replace_mesh(mesh, None, &platform);
+                model.replace_mesh(result.mesh, None, &platform);
+                app.popup.open(Popup::new("Repair Report", move |_app, ui| {
+                    let mut close = false;
+                    ui.label("Mesh repaired successfully, the following defects were found:");
+
+                    ui.add_space(8.0);
+                    grid("repair").show(ui, |ui| {
+                        ui.label("Unwelded Vertices");
+                        ui.horizontal(|ui| {
+                            ui.label(result.unwelded_vertices.to_string());
+                            ui.take_available_width();
+                        });
+                        ui.end_row();
+
+                        ui.label("Holes");
+                        ui.label(result.holes.to_string());
+                        ui.end_row();
+                    });
+                    ui.add_space(8.0);
+
+                    ui.add_space(5.0);
+                    ui.centered_and_justified(|ui| {
+                        close = ui.button("Close").clicked();
+                    });
+                    close
+                }));
+
                 PollResult::complete()
                     .with_task(MeshDefective::new(model))
                     .with_task(BuildAccelerationStructures::new(model))
